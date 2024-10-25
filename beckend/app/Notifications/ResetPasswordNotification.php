577d2f96fdd2d688386cbd40f05514c4185baf82
Subal Roy <subal@mediusware.com>
2024-10-25 13:08:47 +0600

    use queue to send password reset notification

--- beckend/app/Notifications/ResetPasswordNotification.php
+++ beckend/app/Notifications/ResetPasswordNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Lang;
 
-class ResetPasswordNotification extends Notification
+class ResetPasswordNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
