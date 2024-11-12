d3392cd3a14217d395fd77292760b631fd0775da
Subal Roy <subalroy5561@gmail.com>
2024-11-12 13:55:14 +0600

    added fileUpload funtion in Helper

--- beckend/app/Helpers/Helper.php
+++ beckend/app/Helpers/Helper.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
+use PhpParser\Node\Stmt\TryCatch;
 
 function apiResponseWithSuccess(string $message, $data = [], int $code = Response::HTTP_OK)
 {
-    if($data) {
+    if ($data) {
         return response()->json([
             'status' => 'success',
             'message' => $message,
@@ -21,13 +23,13 @@ function apiResponseWithSuccess(string $message, $data = [], int $code = Respons
 
 function apiResponseWithError(mixed $message, $code = 400)
 {
-    if($message instanceof ValidationException) {
+    if ($message instanceof ValidationException) {
         return response()->json([
             'status' => 'error',
             'message' => $message->getMessage(),
             'errors' => $message->errors(),
         ], $code);
-    } else if(is_object($message)) {
+    } else if (is_object($message)) {
         return response()->json([
             'status' => 'error',
             'message' => $message->first(),
@@ -39,5 +41,27 @@ function apiResponseWithError(mixed $message, $code = 400)
             'message' => $message
         ], $code);
     }
+}
 
+if (!function_exists('fileUpload')) {
+    function fileUpload(string $path, $file, string $oldfile =""): string 
+    {
+        if($oldfile && Storage::exists($oldfile)) {
+            Storage::delete($oldfile);
+        }
+        
+        $extension = $file->getClientOriginalExtension();
+        $fileName = date("d-m-Y") . '-' . time();
+        $fileNameWithExtension = $fileName . '.' . $extension;
+
+        try {
+            if(Storage::exists($path . '/' . $fileName . '.' . $extension)) {
+                $fileNameWithExtension = $fileName . (1) . '.' . $extension;
+            }
+
+            return Storage::putFileAs($path, $file, $fileNameWithExtension);
+        } catch (\Throwable $th) {
+            return Storage::putFileAs($path, $file, $fileNameWithExtension);
+        }
+    }
 }
