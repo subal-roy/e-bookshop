768f0be06c5e430dc0aec64b9b9d16cbde3917ae
Subol Roy <102904383+Subol-Roy@users.noreply.github.com>
2024-11-16 17:13:12 +0600

    book details page updated

--- frontend/src/Components/BookDetails/BookDetails.jsx
+++ frontend/src/Components/BookDetails/BookDetails.jsx
@@ -5,17 +5,25 @@ import { MdOutlineGroup } from "react-icons/md";
 import { IoInformationCircleOutline } from "react-icons/io5";
 import { FaCheckCircle } from "react-icons/fa";
 import { SiPowerpages } from "react-icons/si";
+import { FaPenToSquare } from "react-icons/fa6";
+import { FaBookOpen } from "react-icons/fa";
+import { IoQrCode } from "react-icons/io5";
+import { FaCloudArrowDown } from "react-icons/fa6";
+import { FaHandHoldingUsd } from "react-icons/fa";
+import { GiReturnArrow } from "react-icons/gi";
+import { FaHeart } from "react-icons/fa6";
+import { IoShareSocialSharp } from "react-icons/io5";
 
 const BookDetails = () => {
   //const {bookId} = useParams();
   return (
     <div className="w-3/4 m-auto p-5">
       <div className="flex border shadow-md rounded-md">
-        <div className="w-2/3 m-auto bg-white p-5 flex gap-10">
-          <div className="border p-5 rounded-sm">
+        <div className="w-3/4 m-auto bg-white p-5 flex gap-10">
+          <div className="w-1/3 border p-5 rounded-sm">
             <img src={book} alt="#" />
           </div>
-          <div>
+          <div className="2/3">
             <h1 className="text-2xl font-semibold">
               Book Title <span className="text-sm">(format)</span>
             </h1>
@@ -32,38 +40,88 @@ const BookDetails = () => {
               <span>100 Reviews</span>
             </div>
             <div className="flex items-center gap-2 pt-3">
-              <MdOutlineGroup size={20}/>
+              <MdOutlineGroup size={20} />
               <span> 1000 customers want this</span>
-              <IoInformationCircleOutline size={20} className="text-blue-500 cursor-pointer"/>
+              <IoInformationCircleOutline
+                size={20}
+                className="text-blue-500 cursor-pointer"
+              />
             </div>
             <div className="flex items-center gap-2 pt-3 ">
-                <span className="line-through text-gray-500 text-xl">Tk. 200</span>
-                <span className="font-semibold text-xl">Tk. 150</span>
-                <span className="text-green-500">You can save Tk. 50(25%)</span>
+              <span className="line-through text-gray-500 text-xl">
+                Tk. 200
+              </span>
+              <span className="font-semibold text-xl">Tk. 150</span>
+              <span className="text-green-500">You can save Tk. 50(25%)</span>
             </div>
             <div className="flex gap-5 bg-gray-100 border p-3 mt-3 items-center">
-                <img src={book} alt="#" className="h-12 w-8"/>
-                <div className="flex flex-col">
-                    <span className="text-gray-600">Get eBook Version</span>
-                    <span>Tk. 50</span>
-                </div>
-                <button className="h-7 border border-blue-500 text-sm text-blue-500 px-4 rounded-sm hover:bg-blue-500 hover:text-white"><span>Buy to read</span></button>
+              <img src={book} alt="#" className="h-12 w-8" />
+              <div className="flex flex-col">
+                <span className="text-gray-600">Get eBook Version</span>
+                <span>Tk. 50</span>
+              </div>
+              <button className="flex items-center gap-2 h-7 border border-blue-500 text-sm text-blue-500 px-4 rounded-sm hover:bg-blue-500 hover:text-white">
+                <FaCloudArrowDown size={20} />
+                <span>Buy to read</span>
+              </button>
             </div>
             <div className="flex mt-5 items-center gap-2">
-                <FaCheckCircle className="text-green-500" size={20}/>
-                <span>In Stock (50 copies available)</span>
+              <FaCheckCircle className="text-green-500" size={20} />
+              <span>In Stock (50 copies available)</span>
             </div>
-            <div className="mt-5">
-                <div>
-                    <span>Book Length</span>
-                    <SiPowerpages size={30} color="gray"/>
-                </div>
-                <div></div>
-                <div></div>
+            <div className="mt-5 flex gap-5">
+              <div className="flex flex-col items-center gap-1">
+                <span>Book Length</span>
+                <SiPowerpages size={30} color="gray" />
+                <span className="font-semibold">200 pages</span>
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <span>Edition</span>
+                <FaPenToSquare size={30} color="gray" />
+                <span className="font-semibold">2nd Edition</span>
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <span>Publication</span>
+                <FaBookOpen size={30} color="gray" />
+                <span className="font-semibold">ABC publication</span>
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <span>ISBN</span>
+                <IoQrCode size={30} color="gray" />
+                <span className="font-semibold">12345678989</span>
+              </div>
+            </div>
+            <div className="mt-5 flex gap-10">
+              <div className="flex items-center gap-2">
+                <FaHandHoldingUsd />
+                <span>Cash On Delivery</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <GiReturnArrow />
+                <span>7 Days Happy Return</span>
+              </div>
+            </div>
+            <div className="mt-5 flex gap-5">
+              <button className="p-3 text-blue-500 border border-blue-500 rounded-sm hover:text-white hover:bg-blue-500">
+                Read a portion
+              </button>
+              <button className="py-3 px-7 text-blue-500 bg-blue-500 text-white rounded-sm hover:bg-blue-400">
+                Add to Cart
+              </button>
+            </div>
+            <div className="mt-5 flex gap-10">
+              <div className="flex items-center gap-2">
+                <FaHeart size={20} color="gray" />
+                <span>Add to wishlist</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <IoShareSocialSharp size={20} color="gray" />
+                <span>Share this book</span>
+              </div>
             </div>
           </div>
         </div>
-        <div className="w-1/3 m-auto p-2">related books</div>
+        <div className="w-1/4 m-auto p-2">related books</div>
       </div>
     </div>
   );
--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,52 +1,70 @@
 export const login = async (formData) => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      }
+    );
 
     const contentType = response.headers.get("content-type");
 
     if (contentType && contentType.includes("application/json")) {
       const responseData = await response.json();
 
-      if (response.ok) { console.log("responseData:" . responseData);
+      if (response.ok) {
+        console.log("responseData:".responseData);
         return { success: true, data: responseData };
       } else {
-        return { success: false, errors: responseData.errors || { general: responseData.message || "An error occurred. Please try again." } };
+        return {
+          success: false,
+          errors: responseData.errors || {
+            general:
+              responseData.message || "An error occurred. Please try again.",
+          },
+        };
       }
     } else {
       const errorText = await response.text();
       console.error("Expected JSON, received HTML:", errorText);
-      return { success: false, errors: { general: "An unexpected error occurred. Please try again." } };
+      return {
+        success: false,
+        errors: { general: "An unexpected error occurred. Please try again." },
+      };
     }
   } catch (error) {
     console.error(error);
-    return { success: false, errors: { general: "Network error, please try again later." } };
+    return {
+      success: false,
+      errors: { general: "Network error, please try again later." },
+    };
   }
 };
-
 
 // registerApi
 export const register = async (formData) => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/register`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-      body: JSON.stringify({
-        name: formData.name,
-        email: formData.email,
-        password: formData.password,
-        password_confirmation: formData.confirmPassword,
-      }),
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: formData.name,
+          email: formData.email,
+          password: formData.password,
+          password_confirmation: formData.confirmPassword,
+        }),
+      }
+    );
 
     const contentType = response.headers.get("content-type");
 
@@ -59,7 +77,8 @@ export const register = async (formData) => {
         return {
           success: false,
           errors: responseData.errors || {
-            general: responseData.message || "An error occurred. Please try again.",
+            general:
+              responseData.message || "An error occurred. Please try again.",
           },
         };
       }
@@ -73,24 +92,29 @@ export const register = async (formData) => {
     }
   } catch (error) {
     console.error(error);
-    return { success: false, errors: { general: "Network error, please try again later." } };
+    return {
+      success: false,
+      errors: { general: "Network error, please try again later." },
+    };
   }
 };
-
 
 // sendResetPasswordLink
 export const sendResetPasswordLink = async (formData) => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/reset-password-link`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-      body: JSON.stringify({
-        email: formData.email,
-      }),
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/reset-password-link`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          email: formData.email,
+        }),
+      }
+    );
 
     const contentType = response.headers.get("content-type");
 
@@ -103,7 +127,8 @@ export const sendResetPasswordLink = async (formData) => {
         return {
           success: false,
           errors: responseData.errors || {
-            general: responseData.message || "An error occurred. Please try again.",
+            general:
+              responseData.message || "An error occurred. Please try again.",
           },
         };
       }
@@ -117,26 +142,31 @@ export const sendResetPasswordLink = async (formData) => {
     }
   } catch (error) {
     console.error(error);
-    return { success: false, errors: { general: "Network error, please try again later." } };
+    return {
+      success: false,
+      errors: { general: "Network error, please try again later." },
+    };
   }
 };
 
-export const resetPassword  = async (formData) => {
-
+export const resetPassword = async (formData) => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/reset-password`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-      body: JSON.stringify({
-        email: formData.email,
-        password: formData.password,
-        password_confirmation: formData.confirmPassword,
-        token: formData.token
-      }),
-    });
+    const response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/reset-password`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          password: formData.password,
+          password_confirmation: formData.confirmPassword,
+          token: formData.token,
+        }),
+      }
+    );
 
     const contentType = response.headers.get("content-type");
 
@@ -149,7 +179,8 @@ export const resetPassword  = async (formData) => {
         return {
           success: false,
           errors: responseData.errors || {
-            general: responseData.message || "An error occurred. Please try again.",
+            general:
+              responseData.message || "An error occurred. Please try again.",
           },
         };
       }
@@ -163,6 +194,9 @@ export const resetPassword  = async (formData) => {
     }
   } catch (error) {
     console.error(error);
-    return { success: false, errors: { general: "Network error, please try again later." } };
+    return {
+      success: false,
+      errors: { general: "Network error, please try again later." },
+    };
   }
-}
+};
